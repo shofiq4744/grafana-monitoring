8c7062ef766f250f12c83adf61f50b65ea14cfb9
shofiq_loxodon <shofiqul.islam@egeneration.co>
2022-05-28 00:32:40 +0600

    init

---
+++ monitoring-grafana/src/main/java/com/example/monitoringgrafana/MonitoringGrafanaApplication.java
@@ -0,0 +1,13 @@
+package com.example.monitoringgrafana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringGrafanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoringGrafanaApplication.class, args);
+	}
+
+}
---
+++ monitoring-grafana/src/test/java/com/example/monitoringgrafana/MonitoringGrafanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.monitoringgrafana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringGrafanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
